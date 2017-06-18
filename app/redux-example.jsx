32e5cb67e06e63b244684f44c7e4f6aaf237bf0d
Alex Gorelikov <gorelikov.alex@gmail.com>
2017-06-18 16:51:38 +0300

    Location support added

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,10 +1,11 @@
 var redux = require('redux');
+var axios = require('axios');
 
 var nameReducer = (state = '', action) => {
-	switch (action.type) {
+	switch(action.type){
 		case 'CHANGE_NAME':
 			return action.name;
-		default: 
+		default:
 			return state;
 	}
 };
@@ -18,12 +19,12 @@ var changeName = (name) => {
 
 var nextHobbyId = 1;
 var hoobiesReducer = (state = [], action) => {
-	switch(action.type) {
+	switch(action.type){
 		case 'ADD_HOBBY':
 			return [
 				...state,
 				{
-					id: nextHobbyId++,
+					id   : nextHobbyId++,
 					hobby: action.hobby
 				}
 			];
@@ -52,7 +53,7 @@ var removeHobby = (id) => {
 
 var addMovie = (movie) => {
 	return {
-		type: 'ADD_MOVIE',
+		type : 'ADD_MOVIE',
 		title: movie.title,
 		genre: movie.genre
 	}
@@ -67,12 +68,12 @@ var removeMovie = (id) => {
 
 var nextMovieId = 1;
 var moviesReducer = (state = [], action) => {
-	switch (action.type) {
+	switch(action.type){
 		case 'ADD_MOVIE':
 			return [
 				...state,
 				{
-					id: nextMovieId++,
+					id   : nextMovieId++,
 					movie: action.movie
 				}
 			];
@@ -80,19 +81,75 @@ var moviesReducer = (state = [], action) => {
 			return state.filter((movie) => {
 				return movie.id !== action.id;
 			});
-		default: 
+		default:
 			return state;
 	}
 };
 
+var mapReducer = (state = { isFetching: false, url: undefined }, action) => {
+	switch(action.type){
+		case 'START_LOCATION_FETCH':
+			return {
+				isFetching: true,
+				url       : undefined
+			};
+		case 'COMPLETE_LOCATION_FETCH':
+			return {
+				isFetching: false,
+				url       : action.url
+			};
+		default:
+			return state;
+	}
+};
+
+
+var startLocationFetch = () => {
+	return {
+		type: 'START_LOCATION_FETCH'
+	}
+};
+
+var completeLocationFetch = (url) => {
+	return {
+		type: 'COMPLETE_LOCATION_FETCH',
+		url
+	}
+};
+
+var fetchLocation = () => {
+	store.dispatch(startLocationFetch());
+	axios.get('http://ipinfo.io').then((res) => {
+		var location = res.data.loc;
+		var baseUrl = 'http://maps.google.com?q=';
+
+		store.dispatch(completeLocationFetch(baseUrl + location));
+	});
+};
+
 var reducer = redux.combineReducers({
-	                                    name: nameReducer,
+	                                    name   : nameReducer,
 	                                    hobbies: hoobiesReducer,
-	                                    movies: moviesReducer
+	                                    movies : moviesReducer,
+	                                    map    : mapReducer
                                     });
 var store = redux.createStore(reducer, redux.compose(
-window.devToolsExtension ? window.devToolsExtension() : f => f
+window.devToolsExtension? window.devToolsExtension() : f => f
 ));
+
+var unsubscribe = store.subscribe(() => {
+	var state = store.getState();
+
+	if(state.map.isFetching) {
+		document.getElementById('app').innerHTML = 'Loaading';
+	} else if (state.map.url) {
+		document.getElementById('app').innerHTML = '<a href="' + state.map.url + '" target="_blanc">View Your Location</a>';
+	}
+
+	console.log('New State ', store.getState());
+});
+
+fetchLocation();
 
 store.dispatch(changeName('Alexander'));
 store.dispatch(addMovie('Star wars', 'action'));
