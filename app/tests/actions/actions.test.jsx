1b99e76365ac6ee00f5f75245ad65fd6f3bd890d
Alex Gorelikov <gorelikov.alex@gmail.com>
2017-06-23 13:45:38 +0300

    Actions added tests to actions added

---
+++ app/tests/actions/actions.test.jsx
@@ -0,0 +1,47 @@
+var expect = require('expect');
+var actions = require('Actions');
+
+describe('Actions', () => {
+	it('Should exist', () => {
+		expect(actions).toExist();
+	});
+	
+	it('Should generate search text action', () => {
+		var action = {
+			type: 'SET_SEARCH_TEXT',
+			searchText: 'Some generic text'
+		};
+		var res = actions.setSearchText(action.searchText);
+		
+		expect(res).toEqual(action);
+	});
+	
+	it('Should generate addTodo action', () => {
+		var action = {
+			type: 'ADD_TODO',
+			text: 'Some generic todo'
+		};
+		
+		var res = actions.addTodo(action.text);
+		expect(res).toEqual(action);
+	});
+	
+	it('Should generate toggleShowCompleted action', () => {
+		var action = {
+			type: 'TOGGLE_SHOW_COMPLETED'
+		};
+		
+		var res = actions.toggleShowCompleted();
+		expect(res).toEqual(action);
+	});
+	
+	it('Should generate toggleTodo action', () => {
+		var action = {
+			type: 'TOGGLE_TODO',
+			id: 12
+		};
+		
+		var res = actions.toggleTodo(action.id);
+		expect(res).toEqual(action);
+	});
+});
--- webpack.config.js
+++ webpack.config.js
@@ -3,56 +3,59 @@ var path = require('path');
 
 //noinspection JSUnresolvedFunction
 module.exports = {
-	entry: [
-			'script-loader!jquery/dist/jquery.min.js',
-	    'script-loader!foundation-sites/dist/js/foundation.min.js',
-			'./app/app.jsx'
+	entry    : [
+		'script-loader!jquery/dist/jquery.min.js',
+		'script-loader!foundation-sites/dist/js/foundation.min.js',
+		'./app/app.jsx'
 	],
 	externals: {
 		jquery: 'jQuery'
 	},
-	plugins: [
-	new webpack.ProvidePlugin({
-		'$': 'jquery',
-		'jQuery': 'jquery'
-	}),
-	new webpack.DefinePlugin({
-		'process.env': {
-			'NODE_ENV': JSON.stringify('production')
-		} 
-	}),
-	new webpack.LoaderOptionsPlugin({
-		options: {
-			sassLoader: {
-				includePaths: [
-					path.resolve(__dirname, './node_modules/foundation-sites/scss')
-				]
+	plugins  : [
+		new webpack.ProvidePlugin({
+			'$'     : 'jquery',
+			'jQuery': 'jquery'
+		}),
+		new webpack.DefinePlugin({
+			'process.env': {
+				'NODE_ENV': JSON.stringify('production')
 			}
-		}
-	})
+		}),
+		new webpack.LoaderOptionsPlugin({
+			options: {
+				sassLoader: {
+					includePaths: [
+						path.resolve(__dirname, './node_modules/foundation-sites/scss')
+					]
+				}
+			}
+		})
 	],
-	output: {
-		path: __dirname,
+	output   : {
+		path    : __dirname,
 		filename: './public/bundle.js'
 	},
-	resolve: {
-		modules: [__dirname, 'node_modules', './app/components', './app/api'],
-		alias: {
-				ApplicationStyles: 'app/styles/app.scss'
+	resolve  : {
+		modules   : [__dirname, 'node_modules', './app/components', './app/api'],
+		alias     : {
+			ApplicationStyles: 'app/styles/app.scss',
+			Actions          : 'app/actions/actions.jsx'
 		},
 		extensions: ['.js', '.jsx', '.scss']
 	},
-	module: {
-		loaders: [{
-			loader: 'babel-loader',
-			query: {presets: ['react', 'es2015', 'stage-3']},
-			test: /\.jsx?$/,
-			exclude: /(node_modules|bower_components)/
-		},
+	module   : {
+		loaders: [
+			{
+				loader : 'babel-loader',
+				query  : { presets: ['react', 'es2015', 'stage-3'] },
+				test   : /\.jsx?$/,
+				exclude: /(node_modules|bower_components)/
+			},
 			{
 				loader: 'url-loader!file-loader!img-loader?limit=25000',
-				test: /\.(png|jpg)$/
-			}]
+				test  : /\.(png|jpg)$/
+			}
+		]
 	},
-	devtool: 'cheap-module-eval-source-map'
+	devtool  : 'cheap-module-eval-source-map'
 };
